b9a8a0eca55018c5f37e234cfda773779969b34a
Abdul <64336052+ekogeda@users.noreply.github.com>
2022-01-15 23:02:58 +0100

    Update
    Update on staff area

--- employee/private/classes/SalaryDeduction.class.php
+++ employee/private/classes/SalaryDeduction.class.php
@@ -2,12 +2,13 @@
 class SalaryDeduction extends DatabaseObject
 {
   protected static $table_name = "salary_deductions";
-  protected static $db_columns = ['id', 'salary_id', 'tax', 'pension', 'created_at', 'deleted'];
+  protected static $db_columns = ['id', 'salary_id', 'tax', 'pension', 'others', 'created_at', 'deleted'];
 
   public $id;
   public $salary_id;
   public $tax;
   public $pension;
+  public $others;
 
   public $created_at;
   public $deleted;
@@ -19,6 +20,7 @@ class SalaryDeduction extends DatabaseObject
     $this->salary_id        = $args['salary_id'] ?? '';
     $this->tax              = $args['tax'] ?? '';
     $this->pension          = $args['pension'] ?? '';
+    $this->others           = $args['others'] ?? '';
     $this->created_at       = $args['created_at'] ?? date('Y-m-d H:i:s');
     $this->deleted          = $args['deleted'] ?? '';
   }
@@ -36,10 +38,10 @@ class SalaryDeduction extends DatabaseObject
     }
   }
 
-  public static function find_by_deductions()
+  public static function find_by_deductions($salary_id)
   {
-    $sql = "SELECT (tax + pension) AS total_deductions FROM " . static::$table_name . " ";
-    $sql .= " GROUP BY salary_id ";
+    $sql = "SELECT (tax + pension + others) AS total_deductions FROM " . static::$table_name . " ";
+    $sql .= "WHERE salary_id='" . self::$database->escape_string($salary_id) . "'";
     $obj_array = static::find_by_sql($sql);
     if (!empty($obj_array)) {
       return array_shift($obj_array);
