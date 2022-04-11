4293a87a9ada19871baab2ec55612084b75d9aa5
Shafi Akinropo <sakinropo@gmail.com>
2022-04-11 15:03:03 +0100

    push

--- petroleum/sales/inc/create.php
+++ petroleum/sales/inc/create.php
@@ -2,7 +2,6 @@
 
 if (is_post_request()) {
 	$open_stock = $_POST;
-	// pre_r($open_stock);
 	for ($i = 1; $i <= count($open_stock); $i++) {
 		$data = [
 			"tank" 		         => 'tank'.$i,
@@ -14,6 +13,10 @@ if (is_post_request()) {
             "actual_stock"       => $_POST['tank'.$i]['actual_stock'],
             "over_or_short"      => $_POST['tank'.$i]['over_or_short'],
             "exp_sales_value"    => $_POST['tank'.$i]['exp_sales_value'],
+            "cash_submitted" 	 => $_POST['tank'.$i]['cash_submitted'],
+            "total_sales" 		 => $_POST['tank'.$i]['total_sales'],
+            "total_value" 		 => $_POST['tank'.$i]['total_value'],
+            "grand_total"  		 => $_POST['tank'.$i]['grand_total'],
 		];
 
 		// $dataSheet = new DataSheet($data);
