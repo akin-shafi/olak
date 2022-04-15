682afa62ea6e358e31cc6fdf24df209edf0eeb6b
Shafi Akinropo <sakinropo@gmail.com>
2022-04-15 17:22:02 +0100

    push

--- petroleum/private/classes/AccessControl.class.php
+++ petroleum/private/classes/AccessControl.class.php
@@ -3,13 +3,14 @@ class AccessControl extends DatabaseObject
 {
 
 	protected static $table_name = "access_control";
-    protected static $db_columns = ['id','admin_id','support','created_at', 'deleted'];
+    protected static $db_columns = ['id','user_id','user_mgt','report_mgt','created_at', 'deleted'];
 
     public $id;
-    public $admin_id;
-	public $support;
+    public $user_id;
+	  public $user_mgt;
+    public $report_mgt;
     public $created_at;
-	public $deleted;
+	  public $deleted;
 
  
     public $counts;
@@ -17,17 +18,18 @@ class AccessControl extends DatabaseObject
     public function __construct($args=[])
     {
 
-		$this->admin_id 		= $args['admin_id'] ?? '';
-		$this->support 	        = $args['support'] ?? '';
+		$this->user_id 		= $args['user_id'] ?? '';
+    $this->user_mgt           = $args['user_mgt'] ?? '';
+		$this->report_mgt 	        = $args['report_mgt'] ?? '';
 		$this->created_at 		= $args['created_at'] ?? date('Y-m-d H:i:s');
         $this->deleted          = $args['deleted'] ?? '';
     }
 
     
-    public static function find_by_admin_id($admin_id)
+    public static function find_by_user_id($user_id)
     {
         $sql = "SELECT * FROM " . static::$table_name . " ";
-        $sql .= "WHERE admin_id='" . self::$database->escape_string($admin_id) . "'";
+        $sql .= "WHERE user_id='" . self::$database->escape_string($user_id) . "'";
          $sql .= " AND (deleted IS NULL OR deleted = 0 OR deleted = '') ";
         $sql .= "ORDER BY id ASC";
         $obj_array = static::find_by_sql($sql);
