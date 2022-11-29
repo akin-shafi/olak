23ed095df469a30ae7f014e1f291103e920fbaf4
Shafi Akinropo <sakinropo@gmail.com>
2022-11-29 19:29:47 +0100

    Update db_credentials.php

--- procurement/private/db_credentials.php
+++ procurement/private/db_credentials.php
@@ -23,7 +23,7 @@ if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '192.16
 	define("DB_SERVER", "127.0.0.1");
 	define("DB_USER", "hoteliap_ambiance_user");
 	define("DB_PASS", "Akinshafi@91");
-	define("DB_NAME", "hoteliap_olak_hr");
+	define("DB_NAME", "hoteliap_olak_procurement");
 }
 
 
