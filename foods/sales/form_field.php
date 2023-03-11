d5a09560420b5aa242aeb614fb77ab8dd42d7353
Shafi Akinropo <sakinropo@gmail.com>
2023-03-11 14:04:32 +0100

    update

--- foods/sales/form_field.php
+++ foods/sales/form_field.php
@@ -6,7 +6,7 @@
 		<div class="row">
 			<div class="form-group col-12">
 				<label>Date</label>
-				<div><input type="date" name="flow[created_at]" class="form-control" value="<?php echo $cashFlow->created_at ?? ""; ?>" readonly></div>
+				<div><input type="date" name="flow[created_at]" class="form-control" value="<?php echo $cashFlow->created_at ?? ""; ?>" <?php in_array($loggedInAdmin->admin_level, [1,2]) ? "" : "readonly"; ?> ></div>
 			</div>
 			<div class="form-group col-6">
 				<label>Cash Sales</label>
